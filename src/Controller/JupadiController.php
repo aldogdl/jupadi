2679258d4c1eeff2188c3e055c9918341205867b
Aldo Cervantes <aldo.gdl@hotmail.com>
2020-02-25 15:58:30 -0600

    [UPDATE] continuando

--- src/Controller/JupadiController.php
+++ src/Controller/JupadiController.php
@@ -12,8 +12,46 @@ class JupadiController extends AbstractController
      */
     public function index()
     {
-        return $this->render('jupadi/index.html.twig', [
-            'controller_name' => 'JupadiController',
-        ]);
+        return $this->render('jupadi/index.html.twig', []);
+    }
+
+    /**
+     * @Route("contactanos/", name="jupadi-contactanos")
+     */
+    public function contactanos()
+    {
+        return $this->render('jupadi/contacto.html.twig', [ ]);
+    }
+
+    /**
+     * @Route("productos/", name="jupadi-productos")
+     */
+    public function productos()
+    {
+        return $this->render('jupadi/productos.html.twig', [ ]);
+    }
+
+    /**
+     * @Route("jupadi-que-es-flexografia/", name="jupadi-temaFlexo")
+     */
+    public function temaFlexo()
+    {
+        return $this->render('jupadi/tema_flexo.html.twig', [ ]);
+    }
+
+    /**
+     * @Route("jupadi-que-es-el-realzado-en-etiquetas/", name="jupadi-temaRealzado")
+     */
+    public function temaRealzado()
+    {
+        return $this->render('jupadi/tema_realzado.html.twig', [ ]);
+    }
+
+    /**
+     * @Route("jupadi-tipos-de-acabados-en-etiquetas/", name="jupadi-temaAcabados")
+     */
+    public function temaAcavados()
+    {
+        return $this->render('jupadi/tema_acabados.html.twig', [ ]);
     }
 }

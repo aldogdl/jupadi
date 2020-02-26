231d948f34c6e354fe1459ff6fbd8bbec5607db6
Aldo Cervantes <aldo.gdl@hotmail.com>
2020-02-25 19:35:56 -0600

    [FIX] Rutas 1

--- src/Controller/JupadiController.php
+++ src/Controller/JupadiController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class JupadiController extends AbstractController
 {
     /**
-     * @Route("/", name="jupadi-index")
+     * @Route("", name="jupadi-index")
      */
     public function index()
     {
